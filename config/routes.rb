e97211b980660b5e8a9361ba32b41366c01dbd49
Nicolas Roulleau <nicolas.roulleau@gmail.com>
2018-02-01 12:28:55 +0100

    app routée

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   
   resources :corsairs
 
-
-
-  get 'corsair/index', to:'corsairs#index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "static_pages#home"
